df1aad6dee1272f785ab073bb5d3409ab14bf069
dominyka1652 <57347384+dominyka1652@users.noreply.github.com>
2020-04-18 12:08:23 +0300

    Delete v0.3main.cpp

--- v0.3main.cpp
+++
@@ -1,9 +0,0 @@
-#include "v0.3.h"
-
-int main(){
-std::string arZinoKiek; // ar yra zinomas namu darbu skaicius
-std::vector<Studentas> rezultatai;
-rezultatai.reserve(100);
-std::cout << "Ar norite duomenis ivesti ranka, ar nuskaityti is failo?\n Jei ivesti ranka, irasykite 'ivesti', kitu atveju irasykite 'nuskaityti': ";
-duomenys(arZinoKiek,rezultatai);
-}
